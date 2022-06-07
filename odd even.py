bf88a82bd981bc330be3af047da172678f10216c
gurjas14 <106869496+gurjas14@users.noreply.github.com>
2022-06-07 16:07:02 +0530

    Add files via upload

---
+++ odd even.py
@@ -0,0 +1,13 @@
+#Enter a number using custom input
+a = int(input("Enter a number: "))
+
+#If a number is divisible by 2 it returns yes
+if (a%2 == 0):
+    print("even")
+
+#If a number is not divisible by 2 it returns no
+
+else: 
+    print ("odd")
+
+
